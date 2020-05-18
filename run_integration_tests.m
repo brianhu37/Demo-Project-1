1a28fd453664d27d4b1c6e2ce24b2fda3a128607
brianhu <brianhu@ah-brianhu.dhcp.mathworks.com>
2020-05-18 15:54:13 -0400

    Changed run_integration to include path to addOne

--- run_integration_tests.m
+++ run_integration_tests.m
@@ -13,7 +13,7 @@ ARGS{1} = cell(1,1);
 ARGS{1}{1} = coder.typeof(0);
 
 %% Invoke MATLAB Coder.
-codegen -config cfg timesTwo -args ARGS{1}
+codegen -config cfg timesTwo -args ARGS{1} -I 'pwd\addOne'
 
 %% Run the unit tests using the generated MEX file
 coder.runTest('TestTimesTwo', 'timesTwo');
